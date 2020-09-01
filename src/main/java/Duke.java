1466cf63b9b202021966482887fef218ac34b2da
Wang Annan <66814452+AnnanWangDaniel@users.noreply.github.com>
2020-09-01 16:25:19 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printHorizonLine(){
+        System.out.println(" ____________________________________________________________");
+    }
+    public static void printHello(){
+        System.out.println("  Hello! I'm Duke");
+        System.out.println("  What can I do for you?");
+    }
+    public static void printGoodBye(){
+        System.out.println("  Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        */
-        String greet = "____________________________________________________________\n"
-                    + " Hello! I'm Duke\n"
-                    + " What can I do for you?\n"
-                    + "____________________________________________________________\n"
-                    + " Bye. Hope to see you again soon!\n"
-                    + "____________________________________________________________";
-        System.out.println(greet);
+        Scanner k = new Scanner(System.in);
+        String input;
+        printHorizonLine();
+        printHello();
+        printHorizonLine();
+        System.out.println();
+        while(true){
+            input = k.nextLine();
+            if(input.toLowerCase().equals("bye")){
+                break;
+            }
+            printHorizonLine();
+            System.out.println("  " + input);
+            printHorizonLine();
+            System.out.println();
+        }
+        printHorizonLine();
+        printGoodBye();
+        printHorizonLine();
     }
 }
