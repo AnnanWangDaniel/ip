1ca9f96015113da880238aa231a3a26dc3aa8990
Wang Annan <66814452+AnnanWangDaniel@users.noreply.github.com>
2020-09-01 16:57:46 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,52 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static RequestList list = new RequestList();
+
     public static void printHorizonLine(){
         System.out.println(" ____________________________________________________________");
     }
     public static void printHello(){
+        printHorizonLine();
         System.out.println("  Hello! I'm Duke");
         System.out.println("  What can I do for you?");
+        printHorizonLine();
     }
     public static void printGoodBye(){
         System.out.println("  Bye. Hope to see you again soon!");
     }
+
+    public static void printRequestList(){
+        for (int i = 0; i < list.getSize(); i++) {
+            Request t = list.getRequest(i);
+            System.out.println("  " + (i + 1) + ". " + t.getTitle());
+        }
+    }
+
+    public static void addRequest(String title){
+        list.addRequest(new Request(title));
+    }
+
     public static void main(String[] args) {
         Scanner k = new Scanner(System.in);
         String input;
-        printHorizonLine();
         printHello();
-        printHorizonLine();
-        System.out.println();
         while(true){
             input = k.nextLine();
             if(input.toLowerCase().equals("bye")){
                 break;
+            } else if(input.toLowerCase().equals("list")){
+                printHorizonLine();
+                printRequestList();
+                printHorizonLine();
+                continue;
+            }else {
+                addRequest(input);
+                printHorizonLine();
+                System.out.println("  Added : " + input);
+                printHorizonLine();
+                continue;
             }
-            printHorizonLine();
-            System.out.println("  " + input);
-            printHorizonLine();
-            System.out.println();
         }
         printHorizonLine();
         printGoodBye();
---
+++ src/main/java/Request.java
@@ -0,0 +1,49 @@
+public class Request {
+    private String title;
+    private String description;
+    private boolean isDone;
+
+    public Request(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public Request(String title) {
+        this.title = title;
+        this.description = "";
+        this.isDone = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getDone() {
+        return isDone;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDone() {
+        isDone = true;
+    }
+
+    public void setNotDone() {
+        isDone = false;
+    }
+
+    public void print() {
+        System.out.print("[" + (isDone ? "*" : " ") + "] " + title);
+    }
+}
