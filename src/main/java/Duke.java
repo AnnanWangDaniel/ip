250902ab114a3f9450205dd1f8aa10aa19ebb9e2
Wang Annan <66814452+AnnanWangDaniel@users.noreply.github.com>
2020-09-06 23:32:27 +0800

    coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,73 +6,76 @@ public class Duke {
         String line;
         ArrayList<Task> tasks = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        final String HORIZONTAL_LINE = " ____________________________________________________________";
-        System.out.println(HORIZONTAL_LINE);
+        printHorizontalLine();
         System.out.println("  Hello! I'm Duke");
         System.out.println("  What can I do for you?");
-        System.out.println(HORIZONTAL_LINE);
+        printHorizontalLine();
         line = sc.nextLine();
         while (!line.equals("bye")){
             try {
                 if(line.equals("list")){
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     System.out.println("  Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println("  " + (i+1) +":" + tasks.get(i));
                     }
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                 }else if(line.startsWith("done")){
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     int taskNumberCompleted = Integer.parseInt(line.replaceAll("\\D+",""))-1;
                     tasks.get(taskNumberCompleted).completeTask();
                     System.out.println("  Nice! I've marked this task as done:");
                     System.out.println("   "+":"+tasks.get(taskNumberCompleted));
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     System.out.println();
                 } else if(line.startsWith("todo")){
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     System.out.println("  Got it. I've added this task:");
                     tasks.add(new ToDo(line));
                     System.out.println("  " + new ToDo(line));
                     System.out.println("  Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                 } else if(line.startsWith("deadline")){
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     System.out.println("  Got it. I've added this task:");
                     tasks.add(new Deadline(line));
                     System.out.println("  " + new Deadline(line));
                     System.out.println("  Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                 } else if(line.startsWith("event")){
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     System.out.println("  Got it. I've added this task:");
                     tasks.add(new Event(line));
                     System.out.println("  " + new Event(line));
                     System.out.println("  Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                 } else if (line.isEmpty()) {
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     System.out.println("  ☹ OOPS!!! The description of a task cannot be empty.");
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                 } else {
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     System.out.println("  ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    System.out.println(HORIZONTAL_LINE);
+                    printHorizontalLine();
                     System.out.println();
                 }
             } catch (StringIndexOutOfBoundsException e) {
-                System.out.println(HORIZONTAL_LINE);
+                printHorizontalLine();
                 System.out.println("The task you input has missing fields!");
-                System.out.println(HORIZONTAL_LINE);
+                printHorizontalLine();
             } catch (Exception e) {
-                System.out.println(HORIZONTAL_LINE);
+                printHorizontalLine();
                 System.out.println("Something went wrong!");
-                System.out.println(HORIZONTAL_LINE);
+                printHorizontalLine();
             }
             line = sc.nextLine();
         }
-        System.out.println(HORIZONTAL_LINE);
+        printHorizontalLine();
         System.out.println("  Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
+        printHorizontalLine();
+    }
+
+    private static void printHorizontalLine () {
+        System.out.println(" ____________________________________________________________");
     }
 }
